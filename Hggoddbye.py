aedddbccd72a4906c1c35fd9daa1e82dd8c0990b
Alfabehe <110536130+Alfabehe@users.noreply.github.com>
2022-08-03 16:58:11 +0300

    Update and rename README.md to Hggoddbye.py

---
+++ Hggoddbye.py
@@ -0,0 +1,6 @@
+# Welcome-GoodBye
+Code incubation Day #3 
+
+ Birisi Sohbete katıldığında veya ayrıldığında Grupta Hoş Geldiniz / Hoşçakal mesajı göndermek için basit bot  _ _ _ _               
+
+`Just 21 Lines Of code`
